09ce6b082eb12399d959f358017530215c69f3f5
AliciaContreras <alicia.contreras.h@mail.pucv.cl>
2025-11-28 12:19:38 -0300

    corrección del solicitar pedido sin login

--- laboratorio_acme/urls.py
+++ laboratorio_acme/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core import views  # <--- Verifica que esto esté
-from django.contrib.auth import views as auth_views # <--- Verifica que esto esté
+from core import views 
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
